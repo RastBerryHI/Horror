4f0b6f1c9bf7d3855bb0c3d3fc11e0d4f8ced1e1
Musaiev Pasha <pasha.musaiev@gameloft.com>
2022-07-01 16:18:20 +0300

    Fixed doors

--- Assets/Scripts/InterractiveItems/DoorRotator.cs
+++ Assets/Scripts/InterractiveItems/DoorRotator.cs
@@ -5,26 +5,52 @@ public class DoorRotator : InterractiveItem
 {
     private Transform m_Transform;
     private Quaternion baseRotation;
+    private Vector3 openingVector;
+    private Vector3 positiveVector;
     private bool hasRotated;
 
     private void Awake()
     {
         m_Transform = transform;
         baseRotation = m_Transform.rotation;
+
+        openingVector = m_Transform.right * 90;
     }
 
     public override void OnIterraction(GameObject sender)
     {
-        Vector3 senderPos = sender.transform.position;
-        Vector3 doorPos = m_Transform.position;
-
-        Vector3 dir = (senderPos - doorPos).normalized;
-
-        Vector3 targetDir = new Vector3(dir.x, 0, 0);
+        Vector3 invertedLook = -sender.transform.forward;
+        Vector3 euler;
 
         if (!hasRotated)
         {
-            m_Transform.DORotateQuaternion(Quaternion.LookRotation(-sender.transform.forward*90), 1);
+
+            // Eboochiy kosteel            
+            if ( Mathf.Abs( invertedLook.x ) > 0.1f)
+            {
+                // x is positive or not
+                if (invertedLook.x > 0)
+                {
+                    euler = Quaternion.LookRotation(openingVector).eulerAngles;
+                }
+                else
+                {
+                    euler = Quaternion.LookRotation(-openingVector).eulerAngles;
+                }
+            }
+            else
+            {
+                if (invertedLook.z < 0)
+                {
+                    euler = Quaternion.LookRotation(openingVector).eulerAngles;
+                }
+                else
+                {
+                    euler = Quaternion.LookRotation(-openingVector).eulerAngles;
+                }
+            }
+
+            m_Transform.DOLocalRotate(euler, 1); 
             hasRotated = !hasRotated;
         }
         else
