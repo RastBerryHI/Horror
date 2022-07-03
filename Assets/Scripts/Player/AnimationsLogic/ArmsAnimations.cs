be26007a6ea3d291ef04521ca494214279af2ebb
Musaiev Pasha <pasha.musaiev@gameloft.com>
2022-07-03 12:36:18 +0300

    Added interraction for different types of items

--- Assets/Scripts/Player/AnimationsLogic/ArmsAnimations.cs
+++ Assets/Scripts/Player/AnimationsLogic/ArmsAnimations.cs
@@ -6,9 +6,21 @@ public class ArmsAnimations : MonoBehaviour
     [SerializeField] private Animator leftArm;
 
     private int drawOpenId = Animator.StringToHash("DrawOpen");
+    private int doorOpenId = Animator.StringToHash("DoorOpen");
 
-    public void RightArmOpenDraw()
-    {
-        rightArm.SetTrigger(drawOpenId);
+    public void OnInterraction(RaycastHit hit)
+    {   
+        switch(hit.transform.tag)
+        {
+            case "Draw":
+                rightArm.SetTrigger(drawOpenId);
+                break;
+            case "LittleDraw":
+                rightArm.SetTrigger(drawOpenId);
+                break;
+            case "Door":
+                rightArm.SetTrigger(doorOpenId);
+                break;
+        }
     }
 }
